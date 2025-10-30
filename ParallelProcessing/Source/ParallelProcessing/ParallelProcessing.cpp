7ee08d6e726d75f0307ee42b751a25a1f7334fcb
Wiktoria Szramkowska <151945560+wszramkowska@users.noreply.github.com>
2025-10-30 14:31:43 +0000

    added unreal proj

---
+++ ParallelProcessing/Source/ParallelProcessing/ParallelProcessing.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ParallelProcessing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ParallelProcessing, "ParallelProcessing" );
